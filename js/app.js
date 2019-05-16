146eb6e061b8763d53c797e04d3c5f91092e22e0
Casey Falvey <ccfalv@gmail.com>
2019-05-16 10:03:13 -1000

    color change again

--- js/app.js
+++ js/app.js
@@ -7,32 +7,30 @@ var tabs = new Tabby('[data-tabs]');
 (function($) {
   $('#circle-container').circleProgress({
     value: 0.00,
-    size: 120,
+    size: 124,
     startAngle: 2.3,
-    thickness:18,
+    thickness:22,
     animation:100,
-    emptyFill:"rgba(54, 58, 78,1)",
-    fill: {
-      gradient: ["#24bb77","#24bbb4","#24aeb6","#246bba","#5636bb","#7a29bb"]
-    }
+    emptyFill:"#4C5B6D",
+    fill: "#C3EAFF"
   }).on('circle-animation-progress', function(event, progress, stepValue) {
     $(this).find('strong').text(stepValue.toFixed(2).substr(2));
   });
   })(jQuery);
   $(".account-tile").on({
     mouseenter: function () {
-        $('#circle-container').circleProgress('value', 0.10);
+        $('#circle-container').circleProgress('value', 0.30);
     },
     mouseleave: function () {
         $('#circle-container').circleProgress('value', 0.00);
     }
   });
-  $( ".primary-btn" ).click(function() {
-      $('#circle-container').circleProgress('value', 0.10);
+  $( ".primary-btn.spending-btn" ).click(function() {
+      $('#circle-container').circleProgress('value', 0.50);
   });
 
  // Add values to the values array and see what happens :)
-	var values = [0,5,10,15,20,30,40,50,60,70,80,90,100];
+	var values = [0,5,10,15,20,30,40,50,60,70,80,90,100,110];
 
 	drawChart(values,"#chart",10) // You can adjust the margin between each bar by changing 10 to whatever you like
 
